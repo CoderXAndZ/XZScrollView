9d24b8f2025282c6914b5deaa6faef0317f87812
ios_domain <1935722630@qq.com>
2017-10-11 09:11:16 +0800

    Initial Commit

---
+++ XZScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZScrollView
+//
+//  Created by admin on 2017/10/11.
+//  Copyright © 2017年 RongTuoJinRong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZScrollView
+//
+//  Created by admin on 2017/10/11.
+//  Copyright © 2017年 RongTuoJinRong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
