8a714aa20309192e94071bf4070a26917c8a5041
ios_domain <1935722630@qq.com>
2017-10-13 10:15:47 +0800

    存储了缩小的图片高度和宽度，将除了当前图片以外的其他图片进行缩小，但是，滑动的效果不好

--- XZScrollView/XZPageScrollView.m
+++ XZScrollView/XZPageScrollView.m
@@ -24,12 +24,17 @@
     UIScrollView *_scrollview;
     /** 图片数组 */
     NSArray *_arrPics;
+    /** 缩小的图片高度 */
+    CGFloat _reduceHeight;
+    /** 缩小的图片宽度 */
+    CGFloat _reduceWidth;
 }
 
 - (id)initWithFrame:(CGRect)frame target:(id<UIScrollViewDelegate>)target {
     self = [super initWithFrame:frame];
     if (self){
         _scrollview = [[UIScrollView alloc] initWithFrame:CGRectMake(kScreenWidth / 6, 0, kImgWidth, kScreenHeight)];
+        _scrollview.backgroundColor = XZRandomColor;
         _scrollview.pagingEnabled = YES;
         _scrollview.clipsToBounds = NO;
         [self addSubview:_scrollview];
@@ -70,10 +75,15 @@
     for(NSString *url in urls){
         UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(kImgWidth * index, kScreenHeight / 6, kImgWidth, kImgHeight)];
         imgView.backgroundColor = XZRandomColor;
+        NSLog(@"=========%f ========== %f",kScreenWidth,kScreenHeight);
         if (index != 0) {
             CGRect imgBounds = imgView.bounds;
-            imgBounds.size.width =  kImgWidth * 0.2 * (kImgWidth -  fabs(_scrollview.contentOffset.x - kImgWidth * 1) )/ kImgWidth + 0.8 *kImgWidth;
-            imgBounds.size.height =  kImgHeight * 0.2 * (kImgWidth -  fabs(_scrollview.contentOffset.x - kImgWidth * 1) )/ kImgWidth + 0.8 *kImgHeight;
+            //            CGFloat fabsNum = _scrollview.contentOffset.x - kImgWidth * 1;
+            //            kImgWidth * 0.2 * (kImgWidth -  fabs(fabsNum))/ kImgWidth + 0.8 * kImgWidth
+            imgBounds.size.width = kImgWidth - 50 / 375.0 * kScreenWidth;
+            //            kImgHeight * 0.2 * (kImgWidth -  fabs(fabsNum))/ kImgWidth + 0.8 * kImgHeight
+            imgBounds.size.height = kImgHeight - 108 / 812.0 * kScreenHeight;
+            //            NSLog(@"加载图片时的fabsNum：%f======width:%f=====height:%f",fabsNum,imgBounds.size.width,imgBounds.size.height);
             imgView.bounds = imgBounds;
         }
         
@@ -86,39 +96,67 @@
         index++;
     }
     _scrollview.contentSize = CGSizeMake((_scrollview.frame.size.width) * index, 0);
+    _reduceWidth = [self.arrImgView[0] frame].size.width - [self.arrImgView[1] frame].size.width;
+    _reduceHeight = [self.arrImgView[0] frame].size.height - [self.arrImgView[1] frame].size.height;
+    NSLog(@"_reduceWidth:%f---------_reduceHeight:%f",_reduceWidth,_reduceHeight);
 }
 
 #pragma mark ---- 滚动时改变大小
 - (void)scrollViewDidScrolled {
     int index = _scrollview.contentOffset.x / (kImgWidth);
-    if (index == 0) {
-        for (int i = 0; i < 2; i++) {
-            UIImageView *im = _arrImgView[i];
-            CGRect imgBounds = im.bounds;
-            CGFloat fabsNum = _scrollview.contentOffset.x - kImgWidth * i;
-            imgBounds.size.width =  kImgWidth * 0.2 * (kImgWidth -  fabs(fabsNum) )/ (kImgWidth) + 0.8 * kImgWidth;
-            imgBounds.size.height =  kImgHeight * 0.2 * (kImgWidth -  fabs(fabsNum) )/ (kImgWidth) + 0.8 * kImgHeight;
-            im.bounds = imgBounds;
+    
+    for (int i = 0; i < _arrImgView.count; i++) {
+        UIImageView *imgView = _arrImgView[i];
+        imgView.backgroundColor = XZRandomColor;
+        CGRect imgBounds = imgView.bounds;
+        if (i != index) {
+            imgBounds.origin.x = kImgWidth * index + _reduceWidth * 0.5;
+            imgBounds.origin.y = kImgHeight - _reduceHeight * 0.5;
+            imgBounds.size.width = kImgWidth - _reduceWidth;
+            imgBounds.size.height = kImgHeight - _reduceHeight;
+            //            imgBounds = CGRectMake(kImgWidth * index + _reduceWidth * 0.5, kImgHeight - _reduceHeight * 0.5, kImgWidth - _reduceWidth, kImgHeight - _reduceHeight);
+        }else {
+            imgBounds.origin.x = kImgWidth * index;
+            imgBounds.origin.y = kScreenHeight / 6;
+            imgBounds.size.width = kImgWidth;
+            imgBounds.size.height = kImgHeight;
+            
+            //            imgBounds = CGRectMake(kImgWidth * index, kScreenHeight / 6, kImgWidth, kImgHeight);
         }
-    }else if(index == _arrPics.count - 1){
-        for (int i = index - 1; i < index + 1; i++) {
-            UIImageView *imgView = _arrImgView[i];
-            CGRect imgBounds = imgView.bounds;
-            CGFloat fabsNum = _scrollview.contentOffset.x - kImgWidth * i;
-            imgBounds.size.width =  kImgWidth * 0.2 * (kImgWidth -  fabs(fabsNum) )/ (kImgWidth) + 0.8 * kImgWidth;
-            imgBounds.size.height =  kImgHeight * 0.2 * (kImgWidth -  fabs(fabsNum) )/ (kImgWidth) + 0.8 * kImgHeight;
-            imgView.bounds = imgBounds;
-        }
-    }else{
-        for (int i = index - 1; i < index + 2; i++) {
-            UIImageView *imgView = _arrImgView[i];
-            CGRect image = imgView.bounds;
-            CGFloat fabsNum = _scrollview.contentOffset.x - kImgWidth * i;
-            image.size.width =  kImgWidth * 0.2 * (kImgWidth -  fabs(fabsNum) )/ (kImgWidth) + 0.8 * kImgWidth;
-            image.size.height =  kImgHeight * 0.2 * (kImgWidth -  fabs(fabsNum) )/ (kImgWidth) + 0.8 * kImgHeight;
-            imgView.bounds = image;
-        }
+        imgView.bounds = imgBounds;
     }
+    
+    
+    NSLog(@"\n index:%d ==== _scrollview.contentOffset.x:%f",index,_scrollview.contentOffset.x);
+    
+//    if (index == 0) {
+//        for (int i = 0; i < 2; i++) {
+//            UIImageView *im = _arrImgView[i];
+//            CGRect imgBounds = im.bounds;
+//            CGFloat fabsNum = _scrollview.contentOffset.x - kImgWidth * i;
+//            imgBounds.size.width =  kImgWidth * 0.2 * (kImgWidth -  fabs(fabsNum) )/ (kImgWidth) + 0.8 * kImgWidth;
+//            imgBounds.size.height =  kImgHeight * 0.2 * (kImgWidth -  fabs(fabsNum) )/ (kImgWidth) + 0.8 * kImgHeight;
+//            im.bounds = imgBounds;
+//        }
+//    }else if(index == _arrPics.count - 1){
+//        for (int i = index - 1; i < index + 1; i++) {
+//            UIImageView *imgView = _arrImgView[i];
+//            CGRect imgBounds = imgView.bounds;
+//            CGFloat fabsNum = _scrollview.contentOffset.x - kImgWidth * i;
+//            imgBounds.size.width =  kImgWidth * 0.2 * (kImgWidth -  fabs(fabsNum) )/ (kImgWidth) + 0.8 * kImgWidth;
+//            imgBounds.size.height =  kImgHeight * 0.2 * (kImgWidth -  fabs(fabsNum) )/ (kImgWidth) + 0.8 * kImgHeight;
+//            imgView.bounds = imgBounds;
+//        }
+//    }else{
+//        for (int i = index - 1; i < index + 2; i++) {
+//            UIImageView *imgView = _arrImgView[i];
+//            CGRect image = imgView.bounds;
+//            CGFloat fabsNum = _scrollview.contentOffset.x - kImgWidth * i;
+//            image.size.width =  kImgWidth * 0.2 * (kImgWidth -  fabs(fabsNum) )/ (kImgWidth) + 0.8 * kImgWidth;
+//            image.size.height =  kImgHeight * 0.2 * (kImgWidth -  fabs(fabsNum) )/ (kImgWidth) + 0.8 * kImgHeight;
+//            imgView.bounds = image;
+//        }
+//    }
 }
 
 #pragma mark ---- 懒加载
